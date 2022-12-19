85f62e11f216b1c8b940b61faead2c67fd3eb054
RakibFCI <md.rakib3248@gmail.com>
2022-12-19 13:18:27 +0600

    message

---
+++ lib/main.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  String msg = "Flutter Raised Button";
+
+  @override
+  Widget build(BuildContext context) {
+    void _changeText(){
+      setState((){
+        if(msg.startsWith("F")){
+          msg = "We Learn Flutter RaisedButton";
+        }
+        else{
+          msg = "Flutter Raised Button";
+        }
+      });
+    }
+    return SafeArea(
+        child: Scaffold(
+          appBar: AppBar(title: Text("RaisedButton"),),
+          body: Container(
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(msg,style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),),
+                  SizedBox(height: 10,),
+                  RaisedButton(
+                    child: Text("Click",style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),),
+                      color: Colors.deepPurple,
+                      textColor: Colors.yellow,
+                      padding: EdgeInsets.all(20),
+                      onPressed: _changeText),
+                ],
+              ),
+            ),
+          ),
+    ));
+  }
+}
